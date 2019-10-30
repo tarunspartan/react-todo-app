ed7ca0f77ac0b8de342756926f607b63b9272fb5
Tarun M <tarunspartan1234@outlook.com>
2019-10-30 23:06:36 +0530

    Updates

---
+++ precache-manifest.ae110c440056781e9148aa5cf48c75ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b899a88b4dd56b44e8d091c2a3d98a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "c9dc6499f4d8c8f3e733",
+    "url": "/react-todo-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5d87c58f3018e8cf3e02",
+    "url": "/react-todo-app/static/js/2.eca97e7b.chunk.js"
+  },
+  {
+    "revision": "c9dc6499f4d8c8f3e733",
+    "url": "/react-todo-app/static/js/main.0d71f6c6.chunk.js"
+  },
+  {
+    "revision": "058617ebb7442edaa5e3",
+    "url": "/react-todo-app/static/js/runtime-main.2b4dc46c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-todo-app/static/media/logo.25bf045c.svg"
+  }
+]);
